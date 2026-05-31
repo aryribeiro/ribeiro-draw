76efc5a2abc2efcb412728f51003dc2b63c422fc
aryribeiro <aryribeiro@gmail.com>
2026-05-31 12:35:45 -0300

    Corrige file picker para aceitar SVG e PNG ao abrir arquivos

--- packages/excalidraw/data/json.ts
+++ packages/excalidraw/data/json.ts
@@ -104,10 +104,8 @@ export const loadFromJSON = async (
   localElements: readonly ExcalidrawElement[] | null,
 ) => {
   const file = await fileOpen({
-    description: "Excalidraw files",
-    // ToDo: Be over-permissive until https://bugs.webkit.org/show_bug.cgi?id=34442
-    // gets resolved. Else, iOS users cannot open `.excalidraw` files.
-    // extensions: ["json", "excalidraw", "png", "svg"],
+    description: "Ribeiro Draw! files",
+    extensions: ["json", "excalidraw", "png", "svg"],
   });
   return loadFromBlob(file, localAppState, localElements, file.handle);
 };
